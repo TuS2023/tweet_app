bb73bde9c0c1162689c8b8d6e7d3df89ede35591
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-12-18 09:53:39 +0000

    削除、更新、編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get "posts/index" => 'posts#index'
   get "/" => "home#top"
   get "about" => "home#about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id" => "posts#show"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
 end
