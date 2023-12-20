5f1df727e211339a8e8426531a64bd27c41d7b4c
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-12-20 11:09:20 +0000

    ログイン機能の準備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,4 +38,16 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end
+  def login_form
+  end
+  def login
+    @user = User.find_by(email: params[:email],
+                         password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      render("/users/login_form")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
   get "signup" => "users#new"
+  get "login" => "users#login_form"
+  post "login" => "users#login"
   post "users/create" => "users#create"
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
